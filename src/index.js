5662276b5b8213d7a179a7f067e4e8131c093992
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-12-08 23:39:16 +0200

    task1

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import './styles.css';
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const startButtonRef = document.querySelector('button[data-action="start"]');
+const stopButtonRef = document.querySelector('button[data-action="stop"]');
+const bodyRef = document.querySelector('body');
+
+let timerId = null;
+let isButtonStatus = false;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const clickButtonStart = () => {
+  if (isButtonStatus) {
+    return;
+  }
+  isButtonStatus = true;
+  timerId = setInterval(() => {
+    const colorBody = randomIntegerFromInterval(0, 2);
+    bodyRef.style.backgroundColor = colors[colorBody];
+  }, 1000);
+};
+
+const clickButtonStop = () => {
+  isButtonStatus = false;
+  clearInterval(timerId);
+};
+startButtonRef.addEventListener('click', clickButtonStart);
+stopButtonRef.addEventListener('click', clickButtonStop);
